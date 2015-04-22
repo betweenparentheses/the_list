097e58a633350eefa2e9be880b0722b72e8d156f
Michael Alexander <alexandermw@gmail.com>
2015-04-21 21:41:09 -0700

    broken change to uirouter

--- app/assets/javascripts/angular/controllers/ActivitiesIndexCtrl.js
+++ app/assets/javascripts/angular/controllers/ActivitiesIndexCtrl.js
@@ -1,15 +1,15 @@
-theList.controller('ActivitiesIndexCtrl', ['$scope', '$location', '$http', function($scope, $location, $http){
-  $scope.activities = [];
-  $http.get('./activities.json').success(function(data){
-    $scope.activities = data;
-    $scope.categories = data.map(function(elem, i, arr){
-      return elem.category.name;
-    });
-  });
+theList.controller('ActivitiesIndexCtrl',
+ ['$scope',
+ 'activities',
+ 'allActivities',
+ function($scope, activities, allActivities){
+  $scope.activities = activities.activities;
+  $scope.categories = activities.categories;
 
-  $scope.viewActivity = function(id){
-    $location.url("/activities\/" + id );
-  }
+
+  // $scope.viewActivity = function(id){
+  //   $location.url("/activities\/" + id );
+  // }
 
   $scope.name = "";
   $scope.category = "";
--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,22 +1,34 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+'use strict';
+var theList = angular.module('theList', ['ui.router']);
 
-var theList = angular.module('theList', ['ngRoute']);
-
-theList.config(['$routeProvider', function($routeProvider){
-  $routeProvider
-    .when('/activities', {
+theList.config(['$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider){
+  $stateProvider
+    .state('activities', {
+      url: '/activities',
       templateUrl: './templates/activities/index.html',
-      controller: 'ActivitiesIndexCtrl'
+      controller: 'ActivitiesIndexCtrl',
+      resolve: {
+        allActivities: ['activities', function(activities){
+          return activities.getAll();
+        }]
+      }
     })
-    .when('/activities/:id', {
+    .state('activity', {
+      url: '/activities/:id',
       templateUrl: './templates/activities/show.html',
       controller: 'ActivitiesShowCtrl'
     })
-    .otherwise({
+    .state('home', {
+      url: '/',
       templateUrl: './templates/home.html',
       controller: 'HomeCtrl'
     })
+
+    $urlRouterProvider.otherwise('home')
 }]);
 
 
