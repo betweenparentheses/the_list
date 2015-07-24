cea867d1ba1b174f18b2628745271e9bd285ca36
Michael Alexander <alexandermw@gmail.com>
2015-07-24 14:27:00 -0700

    add geocoding to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,11 @@ class Event < ActiveRecord::Base
 
   validates_presence_of :source, :name
   validates_uniqueness_of :name
+
+  geocoded_by :location
+  after_validation :geocode,
+  :if => ->(obj){  obj.location.present? && obj.location_changed? }
+
+
+
 end
